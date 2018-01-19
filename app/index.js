8de6191e2977e0bfbb1da612dab71389a9a17508
Adrian Pothuaud <adrianpothuaud@gmail.com>
2018-01-19 13:49:22 +0000

    added mongoDB connection and list of teams on index page

--- app/index.js
+++ app/index.js
@@ -1,10 +1,32 @@
+// NPM Dependencies
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const MongoClient = require('mongodb').MongoClient
 
-express()
-  .use(express.static('public'))
-  .set('views', 'views')
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Constants
+const PORT = process.env.PORT || 5000
+const app = express()
+
+// vars
+var db
+
+// db connection
+MongoClient.connect("mongodb://owner:esilv123@ds123946.mlab.com:23946/nbanalytics", (err, database) => {
+  if (err) return console.log(err)
+  db = database.db('nbanalytics')
+  // App configuration
+  app
+    .use(express.static('public')) // public folder
+    .set('views', 'views') // views folder
+    .set('view engine', 'ejs') // view engine
+    // routes
+    .get('/', (req, res) => {
+      db.collection('teams').find().toArray((err, result) => {
+        if (err) return console.log(err)
+        // renders index.ejs
+        res.render('pages/index', {teams: result})
+      })
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+})
+
+  
