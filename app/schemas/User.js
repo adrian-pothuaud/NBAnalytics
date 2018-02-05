b0b5f89e35b72a8c1ddb476cef5282fd83378fcd
Adrian Pothuaud <adrianpothuaud@gmail.com>
2018-02-05 14:39:40 +0000

    removed controllers, add password hashing

--- app/schemas/User.js
+++ app/schemas/User.js
@@ -1,9 +1,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passwordHash = require('password-hash');
 
 var UserSchema = new Schema({
     email:  { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true }
+});
+
+UserSchema.pre('save', function(next) {
+    if(!this.isModified()){
+        //not modified
+        next()
+    }
+    this.password = passwordHash.generate(this.password);
+    next();
 });
 
 module.exports = UserSchema;
--- test/User.js
+++ test/User.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var User = require("../app/models/User");
+var passwordHash = require('password-hash');
 
 var assert = require('assert');
 
@@ -15,8 +16,8 @@ describe("User Model", function() {
         })
         .on('error', function(error) {
             console.warn('Error', error);
-        })
-    })
+        });
+    });
 
     it("can list existing users", function(done) {
 
@@ -24,7 +25,7 @@ describe("User Model", function() {
             if (err) return console.error(err);
             assert.notEqual(actions.length, 0);
             done();
-        })
+        });
     });
 
     it("can add a new user", function(done) {
@@ -36,19 +37,32 @@ describe("User Model", function() {
         userX.save(function(err, user) {
             if (err) return console.error(err);
             done();
-        })
-    })
+        });
+    });
+    
+    it("can hash password", function(done) {
+
+        var userX = new User({
+            "email": "test2@test.test",
+            "password": "test123"
+        });
+        userX.save(function(err, user) {
+            if (err) return console.error(err);
+            assert(passwordHash.verify("test123", user.password), "User password not correct");
+            done();
+        });
+    });
     
     it("can update a user", function(done) {
         
-        User.update({"email": {$exists:false}}, { $set: { "email": "test@test.test" }}, {}, done())
-    })
+        User.update({"email": {$exists:false}}, { $set: { "email": "test@test.test" }}, {}, done());
+    });
     
     it("can delete users", function(done) {
         
-        User.find( { $or: [{ "email": "test@test.test" }, {"email": {$exists:false}}]}).remove().exec(function(err, data) {
+        User.find( { $or: [{ "email": "test@test.test" }, { "email": "test2@test.test" },{"email": {$exists:false}}]}).remove().exec(function(err, data) {
             if (err) return console.error(err);
             done();
         });
-    })
+    });
 })
