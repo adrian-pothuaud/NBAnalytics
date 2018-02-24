06e99e8bc8bbbf3b8bfb9002f2bf4d09d949fcd2
Adrian Pothuaud <adrianpothuaud@gmail.com>
2018-02-24 02:14:23 +0000

    add views details

--- app/index.js
+++ app/index.js
@@ -88,9 +88,11 @@ MongoClient.connect("mongodb://owner:esilv123@ds123946.mlab.com:23946/nbanalytic
             if (err) return console.log(err);
             db.collection('actions').find({'GameId': game.GameId}).toArray((err, actions) => {
               if (err) return console.log(err)
-              res.render("pages/gameDetails", {"game": game, 'team1': team1, 'team2': team2, 'actions': actions});
+              db.collection('players').find({}).toArray((err, players) => {
+                if (err) return console.log(err)
+                res.render("pages/gameDetails", {"game": game, 'team1': team1, 'team2': team2, 'actions': actions, 'players': players});
+              })
             })
-            
           })
         })
       })
